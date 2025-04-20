982a24cfc60eaaea60150120dd5156e85e6d3cb6
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2025-04-20 15:13:39 +0200

    add argo-cd

--- kubernetes.tf
+++ kubernetes.tf
@@ -87,16 +87,28 @@ resource "helm_release" "cilium" {
   depends_on = [null_resource.configure_masters]
 }
 
-resource "helm_release" "metrics_server" {
-  name         = "metrics-server"
-  repository   = "https://kubernetes-sigs.github.io/metrics-server"
-  chart        = "metrics-server"
-  force_update = true
-
-  set {
-    name  = "args"
-    value = "{--kubelet-insecure-tls=true}"
-  }
+resource "helm_release" "argo-cd" {
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  force_update     = true
 
   depends_on = [helm_release.cilium]
+}
+
+resource "helm_release" "argocd-apps" {
+  name             = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  create_namespace = true
+  force_update     = true
+
+  values = [
+    "${file("helm/argocd-apps-values.yaml")}"
+  ]
+
+  depends_on = [helm_release.argo-cd]
 }
