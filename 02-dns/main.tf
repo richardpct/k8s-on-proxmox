b35e78cdcdc8520ecea77598209b3d907c0182cc
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2025-09-01 23:23:07 +0200

    add argocd dns

--- 02-dns/main.tf
+++ 02-dns/main.tf
@@ -9,3 +9,11 @@ resource "aws_route53_record" "vault" {
   ttl     = "300"
   records = [var.lb_ip]
 }
+
+resource "aws_route53_record" "argocd" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "argocd"
+  type    = "A"
+  ttl     = "300"
+  records = [var.lb_ip]
+}
