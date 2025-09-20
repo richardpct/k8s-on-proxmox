4fa5a76283514cbb41535245779e6b07dc0d43af
Richard PHAN CHAN THE <43934086+richardpct@users.noreply.github.com>
2025-09-20 21:57:22 +0200

    use list for storing apps

--- 02-dns/main.tf
+++ 02-dns/main.tf
@@ -2,25 +2,10 @@ data "aws_route53_zone" "main" {
   name = var.my_domain
 }
 
-resource "aws_route53_record" "vault" {
+resource "aws_route53_record" "applications" {
+  count   = length(var.applications)
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "vault"
-  type    = "A"
-  ttl     = "300"
-  records = [var.lb_ip]
-}
-
-resource "aws_route53_record" "argocd" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "argocd"
-  type    = "A"
-  ttl     = "300"
-  records = [var.lb_ip]
-}
-
-resource "aws_route53_record" "jenkins" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "jenkins"
+  name    = var.applications[count.index]
   type    = "A"
   ttl     = "300"
   records = [var.lb_ip]
